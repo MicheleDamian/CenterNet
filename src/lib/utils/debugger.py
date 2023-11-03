f5a9a3a4521c9eeb3534a8f26c17ca47cebd3117
Michele Damian <michele.damian@gmail.com>
2023-11-02 21:33:44 -0500

    Add support for tennis player dataset

--- src/lib/utils/debugger.py
+++ src/lib/utils/debugger.py
@@ -43,6 +43,8 @@ class Debugger(object):
         (255, 0, 0), (0, 0, 255)]
     elif num_classes == 80 or dataset == 'coco':
       self.names = coco_class_name
+    elif num_classes == 1 or dataset == 'coco_player':
+      self.names = coco_player_class_name
     elif num_classes == 20 or dataset == 'pascal':
       self.names = pascal_class_name
     elif dataset == 'gta':
@@ -455,6 +457,8 @@ coco_class_name = [
      'oven', 'toaster', 'sink', 'refrigerator', 'book', 'clock', 'vase',
      'scissors', 'teddy bear', 'hair drier', 'toothbrush'
 ]
+
+coco_player_class_name = ['person']
 
 color_list = np.array(
         [
